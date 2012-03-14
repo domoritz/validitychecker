deb0ad4e2fe2c04af89ce98dfe22bbdcd2780fa9
Dominik Moritz <domoritz@gmail.com>
2012-03-14 16:33:59 +0100

    show only finished queries on homepage

--- www/apps/validitychecker/views/index.py
+++ www/apps/validitychecker/views/index.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
@@ -7,7 +10,7 @@ import urllib
 from www.apps.validitychecker.models import Query
 
 def index(request):
-    popular_queries = list(Query.objects.order_by('-count')[:20])
+    popular_queries = list(Query.objects.filter(status=Query.FINISHED).order_by('-count')[:20])
     seed(42)
     shuffle(popular_queries)
     for q in popular_queries:
