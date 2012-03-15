d3b4ae886033b9a58e8e1da5defcbadfecfeef61
Dominik Moritz <domoritz@gmail.com>
2012-03-15 17:38:05 +0100

    refactor scraper

--- www/apps/scrapers/utils/twistedscraper.py
+++ www/apps/scrapers/utils/twistedscraper.py
@@ -6,6 +6,7 @@ from twisted.internet import reactor
 from twisted.web.client import getPage
 from twisted.internet.defer import DeferredList, DeferredSemaphore
 from twisted.python import log
+from twisted.internet.error import ReactorNotRestartable
 
 import logging
 import time, re
@@ -16,11 +17,12 @@ theSemaphore = None
 
 class TwistedScraper():
 
-    def __init__(self, urllist, callback):
+    def __init__(self, urllist, scraper, finished):
         logging.getLogger().setLevel(logging.WARNING)
 
         self.urls = urllist
-        self.callback = callback
+        self.scraper = scraper
+        self.finished = finished
 
     def start(self):
         reactor.callLater(0, self.startFetching)
@@ -48,10 +50,12 @@ class TwistedScraper():
         for success, value in result:
             if success:
                 print 'Success:', value['url']
-                self.callback(value['records'])
+                self.finished(value['records'])
             else:
                 print 'Failure:', value.getErrorMessage()
-        reactor.stop()
+        if reactor.running:
+            reactor.stop()
+
 
     def errorHandler(self, error, url):
         print url
@@ -61,48 +65,7 @@ class TwistedScraper():
         print "processing", url, len(page)
         self.printDelta(startTime)
 
-        parser = etree.HTMLParser()
-        tree = etree.parse(StringIO(page), parser)
-        #result = etree.tostring(tree, pretty_print=True, method="tree")
-
-        elements = tree.xpath("//body/div[@class='gs_r']")
-
-        #log.msg("Found "+str(len(elements))+" article(s)", level=log.INFO)
-
-        # to be returned
-        records = []
-
-        for element in elements:
-
-            a_first = lambda x: x[0] if x else ''
-            a_join = lambda x: ''.join(x)
-            a_split = lambda x: x.split(',')
-
-            a_date = lambda y: date(y, 1, 1)
-
-            def a_find(string, pattern):
-                match = re.search(pattern, string)
-                if match:
-                    return match.group(1)
-                else:
-                    return ''
-
-
-            def perform(element, *functions):
-                result = element
-                for f in functions:
-                    result = f(result)
-                return result
-
-            record = {}
-            record['title'] = perform(element.xpath('h3[@class="gs_rt"]/a//text()'), a_join, unicode)
-            record['url'] = perform(element.xpath('h3[@class="gs_rt"]/a/@href'), a_join, unicode)
-            record['snippet'] = perform(element.xpath('div[@class="gs_rs"]//text()'), a_join, unicode)
-            record['source'] = perform(element.xpath('div[@class="gs_a"]//text()'), a_join, lambda x: a_find(x, r'-\s+(.+)[,|-]\s+\d{4}'),  unicode)
-            record['authors'] = perform(element.xpath('div[@class="gs_a"]//text()'), a_join, lambda x: a_find(x, r'\A(.+?)\s+-\s+'), unicode, a_split)
-            record['publish_date'] = perform(element.xpath('div[@class="gs_a"]//text()'), a_join, lambda x: a_find(x, r'\s+(\d{4})\s+\-'),int ,  a_date)
-
-            records.append(record)
+        records = self.scraper(page)
 
         return {'url':url, 'records':records}
 
@@ -112,9 +75,12 @@ class TwistedScraper():
         return delta
 
     def startReactor(self):
-        if not reactor.running:
-            logging.info('Starting reactor...')
-            reactor.run() # blocking
+        try:
+            if not reactor.running:
+                logging.info('Starting reactor...')
+                reactor.run() # blocking
+        except ReactorNotRestartable as e:
+            print "error", e
         else:
             logging.info('Reactor is already running')
 
