f0c8c99686c0f91c1a92571e44f78ee0061ea43c
Dominik Moritz <domoritz@gmail.com>
2012-03-20 21:49:23 +0100

    refactored scraper tasks

--- www/apps/validitychecker/tasks/scrape.py
+++ www/apps/validitychecker/tasks/scrape.py
@@ -6,21 +6,18 @@ from django.db import transaction
 from celery.task import task, subtask
 
 from www.apps.validitychecker.models import Query, Article, Author
+from www.apps.validitychecker.utils.perform import *
 
 from lxml import etree
-import re, urllib2, urllib
+import urllib2, urllib
 from StringIO import StringIO
-from datetime import date
 
 @task(name='scrape.make_scholar_urls')
 def make_scholar_urls(number, qobj, callback=None):
     query = urllib.quote_plus(qobj.query)
 
-    urls = [
-            'http://scholar.google.com/scholar?as_sdt=1&as_vis=1&num='+str(number)+'&q='+query,
-            'http://scholar.google.com/scholar?as_sdt=1&as_vis=1&num='+str(number)+'&start='+str(number)+'&q='+query,
-            #'http://scholar.google.com/scholar?as_sdt=1&as_vis=1&num='+str(number)+'&start='+str(2*number)+'&q='+query
-            ]
+    step = 80 # max 100
+    urls = ['http://scholar.google.com/scholar?as_sdt=1&as_vis=1&num='+str(step)+'&start='+str(start)+'&q='+query for start in range(0,number,step)]
 
     if callback:
         return [subtask(callback).delay(url, qobj) for url in urls]
@@ -57,41 +54,13 @@ def parse_scholar_page(url, page, qobj, callback=None):
     records = []
 
     for element in elements:
-
-        a_first = lambda x: x[0] if x else ''
-        a_join = lambda x: ''.join(x)
-        a_split = lambda x: x.split(',')
-
-        def a_date(y):
-            if y: # year may be 0 if nothing found
-                d = date(y, 1, 1)
-                return d
-            else:
-                return None
-
-        def a_find(string, pattern):
-            match = re.search(pattern, string)
-            if match:
-                return match.group(1)
-            else:
-                return ''
-
-        a_int = lambda i: int(i) if i else 0
-
-
-        def perform(element, *functions):
-            result = element
-            for f in functions:
-                result = f(result)
-            return result
-
         record = {}
         record['title'] = perform(element.xpath('h3[@class="gs_rt"]/a//text()'), a_join, unicode)
         record['url'] = perform(element.xpath('h3[@class="gs_rt"]/a/@href'), a_join, unicode)
         record['snippet'] = perform(element.xpath('div[@class="gs_rs"]//text()'), a_join, unicode)
         record['source'] = perform(element.xpath('div[@class="gs_a"]//text()'), a_join, lambda x: a_find(x, r'-\s+(.+)[,|-]\s+\d{4}'),  unicode)
-        record['authors'] = perform(element.xpath('div[@class="gs_a"]//text()'), a_join, lambda x: a_find(x, r'\A(.+?)\s+-\s+'), unicode, a_split)
-        record['publish_date'] = perform(element.xpath('div[@class="gs_a"]//text()'), a_join, lambda x: a_find(x, r'\s+(\d{4})\s+\-'),a_int ,  a_date)
+        record['authors'] = perform(element.xpath('div[@class="gs_a"]//text()'), a_join, lambda x: a_find(x, r'\A(.+?)\s+-\s+'), unicode, a_split, m_trim)
+        record['publish_date'] = perform(element.xpath('div[@class="gs_a"]//text()'), a_join, lambda x: a_find(x, r'\s+(\d{4})\s+\-'),a_int, a_date)
 
         records.append(record)
 
@@ -122,8 +91,7 @@ def store_non_credible_in_db(url, records, qobj):
         article.save()
 
         # add author and article to author
-        for author in record['authors']:
-            name = author.strip()
+        for name in record['authors']:
             author, _ = Author.objects.get_or_create(name=name)
             author.articles.add(article)
             author.save()
---
+++ www/apps/validitychecker/utils/perform.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import date
+import re
+
+# a_ for one element
+# m_ for map
+
+a_first = lambda x: x[0] if x else ''
+a_join = lambda x: ''.join(x)
+a_split = lambda x: x.split(',')
+
+def a_date(y):
+    if y: # year may be 0 if nothing found
+        d = date(y, 1, 1)
+        return d
+    else:
+        return None
+
+def a_find(string, pattern):
+    match = re.search(pattern, string)
+    if match:
+        return match.group(1)
+    else:
+        return ''
+
+a_int = lambda i: int(i) if i else 0
+
+def a_trim(n):
+    n = n.strip()
+    n = n.strip(u'…')
+    return n
+
+m_trim = lambda l: map(a_trim, l)
+
+def perform(element, *functions):
+    result = element
+    for f in functions:
+        result = f(result)
+    return result
+
