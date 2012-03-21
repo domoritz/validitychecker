bfdd50d63225c82fb5f550f64d7bcade7222fda8
Dominik Moritz <domoritz@gmail.com>
2012-03-21 14:29:10 +0100

    single db task

--- www/apps/validitychecker/tasks/db.py
+++ www/apps/validitychecker/tasks/db.py
@@ -16,68 +16,31 @@ from www.apps.validitychecker.models import Article, Author
 
 
 #@transaction.commit_on_success
-@task(name='scrape.store_non_credible_in_db')
-def store_non_credible_in_db(url, records, qobj):
+@task(name='scrape.store_in_db')
+def store_in_db(records, qobj, credible=False):
     for record in records:
         # add article
         article, _ = Article.objects.get_or_create(title=record['title'], defaults={'title': record['title']})
-        article.url = record['url']
-        article.snippet = record['snippet']
+
+        article.credible = credible
 
         d = record['publish_date']
         if d:
             article.publish_date = d
 
-        if article.state == Article.INCOMPLETE and article.snippet and article.url and article.publish_date:
-            # set as complete if all interesting things are set
-            article.state = Article.COMPLETE
-        else:
-            article.state = Article.INCOMPLETE
+        if record.has_key('source'):
+            article.source = record['source']
+
+        if record.has_key('url'):
+            article.url = record['url']
+
+        if record.has_key('snippet'):
+            article.snippet = record['snippet']
 
         article.save()
 
         # add author and article to author
         for name in record['authors']:
-            author, _ = Author.objects.get_or_create(name=name)
-            author.articles.add(article)
-            author.save()
-
-        # add article to query
-        qobj.articles.add(article)
-
-    qobj.save()
-
-    return records
-
-
-#@transaction.commit_on_success
-@task(name='fetch.store_credible_in_db')
-def store_credible_in_db(qobj, records):
-    if not records:
-        return []
-
-    for record in records:
-        # add article
-        article, _ = Article.objects.get_or_create(title=record['title'], defaults={'title': record['title'], 'publish_date': record['publish_date']})
-
-        article.is_credible = True # set credible because it's in the isi index
-
-        if article.state in [ Article.INCOMPLETE ]:
-            # fetch isi cited data
-            pass
-
-
-        if article.state == Article.INCOMPLETE and article.snippet and article.url and article.publish_date:
-            # set as complete if all interesting things are set
-            article.state = Article.COMPLETE
-        else:
-            article.state = Article.INCOMPLETE
-
-        article.save()
-
-        # add author and article to author
-        for author in record['authors']:
-            name = ' '.join(reversed(map(unicode.strip, author.split(',')))) # convert name from Doe, J to J Doe
             author, _ = Author.objects.get_or_create(name=name)
             author.articles.add(article)
             author.save()
