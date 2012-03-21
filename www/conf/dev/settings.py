5aa9ecb3ff4bc3e7f6e9e929d1f9f16a119ae1cb
Dominik Moritz <domoritz@gmail.com>
2012-03-21 21:12:09 +0100

    django debug toolbar

--- www/conf/dev/settings.py
+++ www/conf/dev/settings.py
@@ -62,6 +62,19 @@ ADMINS = (
 )
 MANAGERS = ADMINS
 
+#########
+# Debug toolbar
+#########
+
+MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+}
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
